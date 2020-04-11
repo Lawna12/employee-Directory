86eb154be8bb171ab7b3f3ddfd5b0e74888f105c
AndrewL <lawna12@gmail.com>
2020-04-11 19:55:04 -0400

    added AppContext.js for search

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import Header from "./components/Header/header"
 import Table from "./components/Table/table";
 import SearchInput from "./components/SearchInput/searchInput";
+import { AppContext } from "./components/AppContext";
 
 export default function App() {
   return (
-    <div> 
-        <Header />
-        <SearchInput />
-        <Table />
-    </div>
+    <AppContext.Provider value={}>
+      <div> 
+          <Header />
+          <SearchInput />
+          <Table />
+      </div>
+    </AppContext.Provider>
   );
 }
 
