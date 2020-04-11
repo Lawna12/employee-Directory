2384ac49f1268939eff026e5dd96592964045d48
AndrewL <lawna12@gmail.com>
2020-04-11 00:44:52 -0400

    starting over

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,25 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import About from "./pages/About";
-import Search from "./pages/Search";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
+import Header from "./components/Header/header"
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/search" component={Search} />
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App"> 
+        <Header />
+    </div>
   );
 }
 
-export default App;
--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -3,9 +3,13 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
- 
-  getEmployeesList: function() {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");  
+  getRandomDog: function() {
+    return axios.get("https://dog.ceo/api/breeds/image/random");
+  },
+  getDogsOfBreed: function(breed) {
+    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
+  },
+  getBaseBreedsList: function() {
+    return axios.get("https://dog.ceo/api/breeds/list");
   }
-
 };
