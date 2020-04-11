06384e30b08c64d2ebaa7d63026538062f2a54f7
AndrewL <lawna12@gmail.com>
2020-04-11 17:09:08 -0400

    added tableContext.js, and eventContext

--- employee-directory/src/components/Table/table.js
+++ employee-directory/src/components/Table/table.js
@@ -1,39 +1,45 @@
 import React from "react";
 import data from "../../data.json"
-import { TableRow } from "./subComponent/Row/tableRow";
-import TableHeader from "./subComponent/tableHeader/tableHeader";
+import TableRow  from "./subComponent/TableRow/tableRow";
+import TableHeader from "./subComponent/TableHeader/tableHeader";
+import { EventContext } from "./tableContext";
 
 export default function Table() {
+    const handleTableHeaderClick = (id) => {
+        console.log(id);
+    }
     console.log(data)
 
     return (
-        <table border=".5">
-            <thead>
-                <tr>
-                    <TableHeader 
-                        text="Name"
-                    />
-                    <TableHeader 
-                        text="Email"
-                    />
-                    <TableHeader 
-                        text="Phone"
-                    />
-                    <TableHeader 
-                        text="Nationality"
-                    />
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    data.map((person) => (
-                          <TableRow
-                                key={person.phone}
-                                person={person}
-                          />
-                    ))
-                }
-            </tbody>
-        </table>
+        <EventContext.Provider>
+            <table border=".5">
+                <thead>
+                    <tr>
+                        <TableHeader
+                            text="Name"
+                        />
+                        <TableHeader 
+                            text="Email"
+                        />
+                        <TableHeader 
+                            text="Phone"
+                        />
+                        <TableHeader 
+                            text="Nationality"
+                        />
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((person) => (
+                            <TableRow
+                                    key={person.phone}
+                                    person={person}
+                            />
+                        ))
+                    }
+                </tbody>
+            </table>
+        </EventContext.Provider>
     )
 }
